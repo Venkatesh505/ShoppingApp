51ab123c64bdb1ca958da23f6df4be9ebc8c25bc
Venkatesh karamala <venkatesh.karamala@gmail.com>
2019-02-07 15:04:24 -0500

    Exposing Componenet elements to outside world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+serverElements=[{type: 'server',name:"testserver", content:"just a content"}
+,{type: 'blueprint',name:"testserver1", content:"just a content1"}
+,{type: 'server',name:"testserver2", content:"just a content2"}];
 
+  constructor() {    
+   }
+   
 }
