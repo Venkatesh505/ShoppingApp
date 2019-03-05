e5742da5a9d5a49c9c60c02e7b75a6dbd02ebd79
Venkatesh karamala <venkatesh.karamala@gmail.com>
2019-03-05 15:23:50 -0500

    Linking Recipe detail link clicks

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+@Input() recipe: {Name: string, Description: string}
+@Output() ItemClicked= new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  itemclick()
+  {
+    console.log("write")
+    this.ItemClicked.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import{Recipe} from './recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,14 +6,17 @@ import{Recipe} from './recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() Recipelistemit= new EventEmitter<Recipe>();
   recipes: Recipe[]=[
-    new Recipe('First Recipe','Hyderabadi Dum Chicken Biryani','https://www.rachnas-kitchen.com/wp-content/uploads/2016/07/chicken-biryani-@4-1-e1505461223560.jpg')
-    ,new Recipe('First Recipe','Hyderabadi Dum Chicken Biryani','https://www.rachnas-kitchen.com/wp-content/uploads/2016/07/chicken-biryani-@4-1-e1505461223560.jpg')
-    ,new Recipe('First Recipe','Hyderabadi Dum Chicken Biryani','https://www.rachnas-kitchen.com/wp-content/uploads/2016/07/chicken-biryani-@4-1-e1505461223560.jpg')
+    new Recipe('Chicken Recipe','Hyderabadi Dum Chicken Biryani','https://www.rachnas-kitchen.com/wp-content/uploads/2016/07/chicken-biryani-@4-1-e1505461223560.jpg')
+    ,new Recipe('Mutton Recipe','Hyderabadi Dum Mutton Biryani','https://www.swatifood.com/wp-content/uploads/2018/01/Authentic-Hyderabadi-mutton-biryani-recipe.jpg')
+    ,new Recipe('Fish Recipe','Hyderabadi Dum Fish Biryani','https://www.ndtv.com/cooks/images/biryani.4.jpg?downsize=650:400&output-quality=70&output-format=webp')
   ]
   constructor() { }
 
   ngOnInit() {
   }
-
+  rlistClicked(recipe: Recipe){
+this.Recipelistemit.emit(recipe)
+  }
 }
